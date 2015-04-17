4fc8b313949ddc6ee7400c722f4c43d4ea44138d
Jie Chen <jay.jiechen1122@gmail.com>
2015-04-17 01:03:34 -0600

    add product picture and partial for product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < InheritedResources::Base
   private
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :quantity)
+      params.require(:product).permit(:name, :description, :price, :quantity, :image)
     end
 end
 
